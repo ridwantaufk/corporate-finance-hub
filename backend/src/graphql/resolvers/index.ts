cf4e83e33d78e5d730d850d540180d006f740d1e
ridwantaufk <ridwan1998taufik@gmail.com>
2025-05-30 02:21:32 +0700

    modifying resolvers and schemas indexes

--- backend/src/graphql/resolvers/index.ts
+++ backend/src/graphql/resolvers/index.ts
@@ -1,57 +1,59 @@
-import UserResolver from "../modules/auth/users/user.resolvers";
-import TwoFactorAuthResolver from "../modules/auth/two_factor_auth/two_factor_auth.resolvers";
-import ApprovalMatrixResolver from "../modules/auth/approval_matrix/approval_matrix.resolvers";
+import UserResolver from "@/graphql/modules/auth/users/user.resolvers";
+import biodataResolver from "@/graphql/modules/auth/biodata/biodata.resolvers";
+import TwoFactorAuthResolver from "@/graphql/modules/auth/two_factor_auth/two_factor_auth.resolvers";
+import ApprovalMatrixResolver from "@/graphql/modules/auth/approval_matrix/approval_matrix.resolvers";
 
-import VirtualAccountResolver from "../modules/cash_management/virtual_accounts/virtual_accounts.resolvers";
-import CashPositionResolver from "../modules/cash_management/cash_positions/cash_positions.resolvers";
-import ScheduledPaymentResolver from "../modules/cash_management/scheduled_payments/scheduled_payments.resolvers";
+// import VirtualAccountResolver from "@/graphql/modules/cash_management/virtual_accounts/virtual_accounts.resolvers";
+// import CashPositionResolver from "@/graphql/modules/cash_management/cash_positions/cash_positions.resolvers";
+// import ScheduledPaymentResolver from "@/graphql/modules/cash_management/scheduled_payments/scheduled_payments.resolvers";
 
-import BulkPaymentResolver from "../modules/payments_collections/bulk_payments/bulk_payments.resolvers";
-import InvoiceResolver from "../modules/payments_collections/invoices/invoices.resolvers";
-import ReconciliationResolver from "../modules/payments_collections/reconciliations/reconciliations.resolvers";
+// import BulkPaymentResolver from "@/graphql/modules/payments_collections/bulk_payments/bulk_payments.resolvers";
+// import InvoiceResolver from "@/graphql/modules/payments_collections/invoices/invoices.resolvers";
+// import ReconciliationResolver from "@/graphql/modules/payments_collections/reconciliations/reconciliations.resolvers";
 
-import LetterOfCreditResolvers from "../modules/trade_finance/letter_of_credit/letter_of_credit.resolvers";
-import InvoiceFinancingResolver from "../modules/trade_finance/invoice_financing/invoice_financing.resolvers";
-import ImportExportTrackingResolver from "../modules/trade_finance/import_export_tracking/import_export_tracking.resolvers";
+// import LetterOfCreditResolvers from "@/graphql/modules/trade_finance/letter_of_credit/letter_of_credit.resolvers";
+// import InvoiceFinancingResolver from "@/graphql/modules/trade_finance/invoice_financing/invoice_financing.resolvers";
+// import ImportExportTrackingResolver from "@/graphql/modules/trade_finance/import_export_tracking/import_export_tracking.resolvers";
 
-import FxTransactionsResolver from "../modules/treasury_fx/fx_transactions/fx_transactions.resolvers";
-import IntercompanySettlementsResolver from "../modules/treasury_fx/intercompany_settlements/intercompany_settlements.resolvers";
+// import FxTransactionsResolver from "@/graphql/modules/treasury_fx/fx_transactions/fx_transactions.resolvers";
+// import IntercompanySettlementsResolver from "@/graphql/modules/treasury_fx/intercompany_settlements/intercompany_settlements.resolvers";
 
-import EFakturResolver from "../modules/tax_compliance/e_faktur/e_faktur.resolvers";
-import RegulatoryReportsResolvers from "../modules/tax_compliance/regulatory_reports/regulatory_reports.resolvers";
-import AmlChecksResolvers from "../modules/tax_compliance/aml_checks/aml_checks.resolvers";
+// import EFakturResolver from "@/graphql/modules/tax_compliance/e_faktur/e_faktur.resolvers";
+// import RegulatoryReportsResolvers from "@/graphql/modules/tax_compliance/regulatory_reports/regulatory_reports.resolvers";
+// import AmlChecksResolvers from "@/graphql/modules/tax_compliance/aml_checks/aml_checks.resolvers";
 
-import CashFlowForecastsResolver from "../modules/analytics/cash_flow_forecasts/cash_flow_forecasts.resolvers";
-import TransactionRiskScoresResolver from "../modules/analytics/transaction_risk_scores/transaction_risk_scores.resolvers";
-import TransactionHeatmapResolver from "../modules/analytics/transaction_heatmap/transaction_heatmap.resolvers";
+// import CashFlowForecastsResolver from "@/graphql/modules/analytics/cash_flow_forecasts/cash_flow_forecasts.resolvers";
+// import TransactionRiskScoresResolver from "@/graphql/modules/analytics/transaction_risk_scores/transaction_risk_scores.resolvers";
+// import TransactionHeatmapResolver from "@/graphql/modules/analytics/transaction_heatmap/transaction_heatmap.resolvers";
 
 const resolvers = [
   UserResolver,
+  biodataResolver,
   TwoFactorAuthResolver,
   ApprovalMatrixResolver,
 
-  VirtualAccountResolver,
-  CashPositionResolver,
-  ScheduledPaymentResolver,
+  // VirtualAccountResolver,
+  // CashPositionResolver,
+  // ScheduledPaymentResolver,
 
-  BulkPaymentResolver,
-  InvoiceResolver,
-  ReconciliationResolver,
+  // BulkPaymentResolver,
+  // InvoiceResolver,
+  // ReconciliationResolver,
 
-  LetterOfCreditResolvers,
-  InvoiceFinancingResolver,
-  ImportExportTrackingResolver,
+  // LetterOfCreditResolvers,
+  // InvoiceFinancingResolver,
+  // ImportExportTrackingResolver,
 
-  FxTransactionsResolver,
-  IntercompanySettlementsResolver,
+  // FxTransactionsResolver,
+  // IntercompanySettlementsResolver,
 
-  EFakturResolver,
-  RegulatoryReportsResolvers,
-  AmlChecksResolvers,
+  // EFakturResolver,
+  // RegulatoryReportsResolvers,
+  // AmlChecksResolvers,
 
-  CashFlowForecastsResolver,
-  TransactionRiskScoresResolver,
-  TransactionHeatmapResolver,
+  // CashFlowForecastsResolver,
+  // TransactionRiskScoresResolver,
+  // TransactionHeatmapResolver,
 ];
 
 export default resolvers;
--- backend/src/graphql/schemas/index.ts
+++ backend/src/graphql/schemas/index.ts
@@ -1,57 +1,59 @@
-import UserTypeDefs from "../modules/auth/users/user.schema";
-import TwoFactorAuthTypeDefs from "../modules/auth/two_factor_auth/two_factor_auth.schema";
-import ApprovalMatrixTypeDefs from "../modules/auth/approval_matrix/approval_matrix.schema";
+import UserTypeDefs from "@/graphql/modules/auth/users/user.schema";
+import BiodataTypeDefs from "@/graphql/modules/auth/biodata/biodata.schema";
+import TwoFactorAuthTypeDefs from "@/graphql/modules/auth/two_factor_auth/two_factor_auth.schema";
+import ApprovalMatrixTypeDefs from "@/graphql/modules/auth/approval_matrix/approval_matrix.schema";
 
-import VirtualAccountTypeDefs from "../modules/cash_management/virtual_accounts/virtual_accounts.schema";
-import CashPositionTypeDefs from "../modules/cash_management/cash_positions/cash_positions.schema";
-import ScheduledPaymentTypeDefs from "../modules/cash_management/scheduled_payments/scheduled_payments.schema";
+// import VirtualAccountTypeDefs from "@/graphql/modules/cash_management/virtual_accounts/virtual_accounts.schema";
+// import CashPositionTypeDefs from "@/graphql/modules/cash_management/cash_positions/cash_positions.schema";
+// import ScheduledPaymentTypeDefs from "@/graphql/modules/cash_management/scheduled_payments/scheduled_payments.schema";
 
-import BulkPaymentTypeDefs from "../modules/payments_collections/bulk_payments/bulk_payments.schema";
-import InvoiceTypeDefs from "../modules/payments_collections/invoices/invoices.schema";
-import ReconciliationTypeDefs from "../modules/payments_collections/reconciliations/reconciliations.schema";
+// import BulkPaymentTypeDefs from "@/graphql/modules/payments_collections/bulk_payments/bulk_payments.schema";
+// import InvoiceTypeDefs from "@/graphql/modules/payments_collections/invoices/invoices.schema";
+// import ReconciliationTypeDefs from "@/graphql/modules/payments_collections/reconciliations/reconciliations.schema";
 
-import LetterOfCreditTypeDefs from "../modules/trade_finance/letter_of_credit/letter_of_credit.schema";
-import InvoiceFinancingTypeDefs from "../modules/trade_finance/invoice_financing/invoice_financing.schema";
-import ImportExportTrackingTypeDefs from "../modules/trade_finance/import_export_tracking/import_export_tracking.schema";
+// import LetterOfCreditTypeDefs from "@/graphql/modules/trade_finance/letter_of_credit/letter_of_credit.schema";
+// import InvoiceFinancingTypeDefs from "@/graphql/modules/trade_finance/invoice_financing/invoice_financing.schema";
+// import ImportExportTrackingTypeDefs from "@/graphql/modules/trade_finance/import_export_tracking/import_export_tracking.schema";
 
-import FxTransactionsTypeDefs from "../modules/treasury_fx/fx_transactions/fx_transactions.schema";
-import IntercompanySettlementsTypeDefs from "../modules/treasury_fx/intercompany_settlements/intercompany_settlements.schema";
+// import FxTransactionsTypeDefs from "@/graphql/modules/treasury_fx/fx_transactions/fx_transactions.schema";
+// import IntercompanySettlementsTypeDefs from "@/graphql/modules/treasury_fx/intercompany_settlements/intercompany_settlements.schema";
 
-import EFakturTypeDefs from "../modules/tax_compliance/e_faktur/e_faktur.schema";
-import RegulatoryReportsTypeDefs from "../modules/tax_compliance/regulatory_reports/regulatory_reports.schema";
-import AmlChecksTypeDefs from "../modules/tax_compliance/aml_checks/aml_checks.schema";
+// import EFakturTypeDefs from "@/graphql/modules/tax_compliance/e_faktur/e_faktur.schema";
+// import RegulatoryReportsTypeDefs from "@/graphql/modules/tax_compliance/regulatory_reports/regulatory_reports.schema";
+// import AmlChecksTypeDefs from "@/graphql/modules/tax_compliance/aml_checks/aml_checks.schema";
 
-import CashFlowForecastsTypeDefs from "../modules/analytics/cash_flow_forecasts/cash_flow_forecasts.schema";
-import TransactionRiskScoresTypeDefs from "../modules/analytics/transaction_risk_scores/transaction_risk_scores.schema";
-import TransactionHeatmapTypeDefs from "../modules/analytics/transaction_heatmap/transaction_heatmap.schema";
+// import CashFlowForecastsTypeDefs from "@/graphql/modules/analytics/cash_flow_forecasts/cash_flow_forecasts.schema";
+// import TransactionRiskScoresTypeDefs from "@/graphql/modules/analytics/transaction_risk_scores/transaction_risk_scores.schema";
+// import TransactionHeatmapTypeDefs from "@/graphql/modules/analytics/transaction_heatmap/transaction_heatmap.schema";
 
 const typeDefs = [
   UserTypeDefs,
+  BiodataTypeDefs,
   TwoFactorAuthTypeDefs,
   ApprovalMatrixTypeDefs,
 
-  VirtualAccountTypeDefs,
-  CashPositionTypeDefs,
-  ScheduledPaymentTypeDefs,
+  // VirtualAccountTypeDefs,
+  // CashPositionTypeDefs,
+  // ScheduledPaymentTypeDefs,
 
-  BulkPaymentTypeDefs,
-  InvoiceTypeDefs,
-  ReconciliationTypeDefs,
+  // BulkPaymentTypeDefs,
+  // InvoiceTypeDefs,
+  // ReconciliationTypeDefs,
 
-  LetterOfCreditTypeDefs,
-  InvoiceFinancingTypeDefs,
-  ImportExportTrackingTypeDefs,
+  // LetterOfCreditTypeDefs,
+  // InvoiceFinancingTypeDefs,
+  // ImportExportTrackingTypeDefs,
 
-  FxTransactionsTypeDefs,
-  IntercompanySettlementsTypeDefs,
+  // FxTransactionsTypeDefs,
+  // IntercompanySettlementsTypeDefs,
 
-  EFakturTypeDefs,
-  RegulatoryReportsTypeDefs,
-  AmlChecksTypeDefs,
+  // EFakturTypeDefs,
+  // RegulatoryReportsTypeDefs,
+  // AmlChecksTypeDefs,
 
-  CashFlowForecastsTypeDefs,
-  TransactionRiskScoresTypeDefs,
-  TransactionHeatmapTypeDefs,
+  // CashFlowForecastsTypeDefs,
+  // TransactionRiskScoresTypeDefs,
+  // TransactionHeatmapTypeDefs,
 ];
 
 export default typeDefs;
