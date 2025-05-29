fe5d6f11dffb789499c501848fda74b499c77cf9
ridwantaufk <ridwan1998taufik@gmail.com>
2025-05-29 22:31:43 +0700

    adding authMiddleware and logReqMiddleware

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -1,17 +1,43 @@
-import { verifyToken } from "../utils/auth";
+// src/middleware/authMiddleware.ts
+import { Response, NextFunction } from "express";
+import { verifyToken, verifyTokenJwtRS256 } from "@/utils/auth";
+import { RequestWithUser } from "@/types";
 
-export const authMiddleware = (req: any, res: any, next: any) => {
-  const token = req.cookies.accessToken;
+/**
+ * Middleware autentikasi untuk mengekstrak dan memverifikasi token JWT dari cookies
+ * Mengatur req.user dengan data user yang terautentikasi atau null jika tidak ada token
+ */
+export const authMiddleware = (
+  req: RequestWithUser,
+  res: Response,
+  next: NextFunction
+): void => {
+  // pake ini biar lebih aman dan efisien, karena pengecekan tanpa set dari user, alias tdk manual pengisian token cookiesnya, karena dia otomatis ambil dari cookies yang nempel di client (cek: devTools->appliaction->cookies)
+  const token = req.cookies?.token;
+  // console.log("Auth Middleware: Checking token in cookies", token);
+
+  // // fungsi req.headers.authorization ini untuk permintaan header dari client, biasanya untuk REST API, tdk menutup kemungkinan juga bisa digunakan untuk GraphQL (misal: fetch API dengan pengisian token di header sisi client/frontend/ssr),  kekurangannya ini perlu diset manual oleh pengembang di sisi client, jadi tidak otomatis seperti cookies, dan juga tidak aman karena bisa saja tokennya diubah oleh user, jadi lebih baik pakai cookies yang sudah di set dari server
+  // const authorized = req.headers.authorization;
+  // console.log(
+  //   "Auth Middleware: Checking authentication in cookies",
+  //   authorized
+  // );
 
   if (!token) {
-    return res.status(401).send("Authentication required");
+    req.user = null;
+    return next();
   }
 
   try {
-    const decoded = verifyToken(token);
-    req.user = decoded;
+    // opsi 1
+    // const decoded = verifyToken(token);
+
+    // opsi 2
+    const decoded = verifyTokenJwtRS256(token);
+    req.user = decoded || null;
     next();
   } catch (error) {
-    return res.status(401).send("Invalid or expired token");
+    req.user = null;
+    next();
   }
 };
